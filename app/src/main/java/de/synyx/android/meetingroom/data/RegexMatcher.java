e99d09136e5135202e2ed6b638e3db6ebddf9975
Max Dobler <dobler@synyx.de>
2019-03-07 10:10:30 +0100

    Fix meeting room name regex

--- app/src/main/java/de/synyx/android/meetingroom/data/RegexMatcher.java
+++ app/src/main/java/de/synyx/android/meetingroom/data/RegexMatcher.java
@@ -24,7 +24,7 @@ class RegexMatcher {
 
         try {
             return Integer.valueOf(matcher.group(0));
-        } catch (NumberFormatException exception) {
+        } catch (NumberFormatException | IllegalStateException exception) {
             return null;
         }
     }
