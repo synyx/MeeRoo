deffd4263a336464d0a1a56f9f1ea665d8f3b2ee
Julian Heetel <julian.heetel@gmail.com>
2019-11-20 16:18:01 +0100

    WIP: Refactor login
    ToDo:
    * check if login completed before displaying login UI
    * extract string resources
    * Fix MainActivity theme by separating themes
    * Filter selectable accounts calendar support
    * Test with CalDAV

--- app/src/main/java/de/synyx/android/meeroo/screen/login/mvvm/MVVMLoginViewModel.java
+++ app/src/main/java/de/synyx/android/meeroo/screen/login/mvvm/MVVMLoginViewModel.java
@@ -1,9 +1,17 @@
 package de.synyx.android.meeroo.screen.login.mvvm;
 
+import android.accounts.AccountManager;
+
+import android.content.Intent;
+
+import android.text.TextUtils;
+
 import androidx.fragment.app.Fragment;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import de.synyx.android.meeroo.domain.CalendarMode;
 
 import java.util.List;
 
@@ -48,8 +56,35 @@ public class MVVMLoginViewModel extends ViewModel {
     }
 
 
-    public MutableLiveData<LoginStep> getLoginStep() {
+    MutableLiveData<LoginStep> getLoginStep() {
 
         return loginStep;
+    }
+
+
+    void stepAccount(Fragment fragment) {
+
+        if (!config.getPreferencesService().isLoggedIn()) {
+            Intent accountIntent = AccountManager.newChooseAccountIntent(null, null, null, null, null, null, null);
+            fragment.startActivityForResult(accountIntent, MVVMLoginFragment.REQUEST_ACCOUNT);
+        } else {
+            loginStep.postValue(LoginStep.MODE);
+        }
+    }
+
+
+    void stepMode() {
+
+        if (!TextUtils.isEmpty(config.getPreferencesService().getSelectedCalenderMode())) {
+            loginStep.postValue(LoginStep.FINISHED);
+        }
+    }
+
+
+    void saveCalendarMode(CalendarMode mode) {
+
+        String modeString = config.getCalendarModeService().getStringCalenderMode(mode);
+        config.getPreferencesService().saveCalendarMode(modeString);
+        loginStep.postValue(LoginStep.FINISHED);
     }
 }
