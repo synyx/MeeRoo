5ecff51c84dbdda2790532054b5b2455aae7e7c9
Julian Heetel <julian.heetel@gmail.com>
2019-12-10 14:12:48 +0100

    Skip LoginActivity when already logged in

--- app/src/main/java/de/synyx/android/meeroo/screen/login/mvvm/MVVMLoginActivity.java
+++ app/src/main/java/de/synyx/android/meeroo/screen/login/mvvm/MVVMLoginActivity.java
@@ -1,6 +1,10 @@
 package de.synyx.android.meeroo.screen.login.mvvm;
 
+import android.content.Intent;
+
 import android.os.Bundle;
+
+import android.text.TextUtils;
 
 import androidx.annotation.Nullable;
 
@@ -10,6 +14,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import de.synyx.android.meeroo.R;
+import de.synyx.android.meeroo.config.Registry;
+import de.synyx.android.meeroo.preferences.PreferencesService;
+import de.synyx.android.meeroo.screen.main.MainActivity;
+import de.synyx.android.meeroo.util.proxy.PermissionManager;
 
 
 /**
@@ -23,6 +31,13 @@ public class MVVMLoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        if (isSetupCompleted()) {
+            startMainActivity();
+
+            return;
+        }
+
         setContentView(R.layout.activity_login);
 
         FragmentManager manager = getSupportFragmentManager();
@@ -32,5 +47,23 @@ public class MVVMLoginActivity extends AppCompatActivity {
             fragment = new MVVMLoginFragment();
             manager.beginTransaction().replace(R.id.content_main, fragment, FRAGMENT_TAG).commit();
         }
+    }
+
+
+    private boolean isSetupCompleted() {
+
+        PreferencesService preferencesService = Registry.get(PreferencesService.class);
+        PermissionManager permissionManager = Registry.get(PermissionManager.class);
+
+        return preferencesService.isLoggedIn() && permissionManager.getNeededPermissions().isEmpty()
+            && !TextUtils.isEmpty(preferencesService.getSelectedCalenderMode());
+    }
+
+
+    void startMainActivity() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
